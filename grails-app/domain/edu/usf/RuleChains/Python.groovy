1f958e5163e306f3ff7b7b75ae628297ed58eed7
James Jones <jamjon3@gmail.com>
2014-09-26 09:33:36 -0400

    Created the Python rule domain class

---
+++ grails-app/domain/edu/usf/RuleChains/Python.groovy
@@ -0,0 +1,57 @@
+package edu.usf.RuleChains
+
+/**
+ * Python extends the basic Rule domain class and is the unit
+ * for processing a rule containing text in the Jython language.
+ * <p>
+ * Developed originally for the University of South Florida
+ * 
+ * @author <a href='mailto:james@mail.usf.edu'>James Jones</a> 
+ */ 
+class Python extends Rule {
+    String rule = ""
+    static constraints = {
+    }
+    static mapping = {
+        rule type: 'text'
+    }
+    /*
+     * Handles syncronization for saves 
+     */
+    def afterInsert() {
+        if(isSynced) {
+            saveGitWithComment("Creating ${name} Groovy")
+        }
+    }
+    /*
+     * Handles syncronization for update
+     */
+    def beforeUpdate() {
+        if(isSynced) {
+            updateGitWithComment("Renaming ${name} Groovy")
+        }
+    }
+    /*
+     * Handles syncronization for post-update saves 
+     */    
+    def afterUpdate() {
+        if(isSynced) {
+            saveGitWithComment("Updating ${name} Groovy")
+            /**
+             * Anytime a rule is renamed, any link referenced rule name in git repo needs to be updated (if exists)
+             **/
+            Link.findAllByRule(this).each { l ->
+                l.saveGitWithComment("Updating Link referencing ${name} SQLQuery")
+            }
+        }
+    }
+    /*
+     * Handles syncronization for deletes 
+     */    
+    def beforeDelete() {
+        if(isSynced) {
+            deleteGitWithComment("Deleted ${name} Groovy")
+        }
+    }
+    
+}
