8ea506026ad727271fe25d0558b04904640211f1
James Jones <jamjon3@gmail.com>
2014-10-01 10:52:04 -0400

    Added a Camel JCIFS RouteBuilder method that takes a closure and passes in the generated routeBuilder

--- grails-app/services/edu/usf/RuleChains/GroovyRuleEnvironmentService.groovy
+++ grails-app/services/edu/usf/RuleChains/GroovyRuleEnvironmentService.groovy
@@ -2,6 +2,7 @@ package edu.usf.RuleChains
 
 import groovy.sql.Sql
 import oracle.jdbc.driver.OracleTypes
+import org.apache.camel.builder.RouteBuilder
 import groovy.text.*
 import grails.converters.*
 import edu.usf.cims.emailer.EmailerEngine
@@ -20,5 +21,19 @@ class GroovyRuleEnvironmentService {
     def sendEmail(ConfigObject config, String emailText) {
         (new EmailerEngine()).sendEmail(config,emailText)
     }
-    
+    /**
+     * Implements Camel JCIFS to create a RouteBuilder and pass it to a closure
+     * 
+     * @param   smbUrl    A JCIFS formatted url (http://camel.apache.org/jcifs.html)
+     * @param   targetDir  A directory to syncronize (aka: "file://tmp/mysmbstuff")
+     * @see     RouteBuilder
+     */
+    def createCIFSRouteBuilder(String smbUrl,String targetDir,Closure routeBuilderClosure) {
+        routeBuilderClosure.call(new RouteBuilder() {
+            public void configure() throws Exception {
+                from(smbUrl)
+                    .to(targetDir);
+            }
+        })        
+    }
 }
