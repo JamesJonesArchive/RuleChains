d624dd0e38a3d62978bf7454536c63993edd8fe4
James Jones <jamjon3@gmail.com>
2014-10-08 11:00:44 -0400

    Lot's of changes. Removed some unneeded methods. Rewrote methods to use the new log appender domain class away from the manual process

--- grails-app/services/edu/usf/RuleChains/JobService.groovy
+++ grails-app/services/edu/usf/RuleChains/JobService.groovy
@@ -24,39 +24,6 @@ class JobService {
     def grailsApplication
     def userInfoHandlerService
     /**
-     * Adds a new Job History
-     * 
-     * @param    name    The unique name of the job history
-     * @return           An object containing the job history
-     */
-    def addJobHistory(String name) {
-        if(!!name) {
-            def jobHistory = [ name: name.trim() ] as JobHistory
-            if(!jobHistory.save(failOnError:false, flush: true, insert: true, validate: true)) {
-                return [ error : "'${jobHistory.errors.fieldError.field}' value '${jobHistory.errors.fieldError.rejectedValue}' rejected" ]
-            } else {
-                return [ jobHistory: jobHistory ]
-            }
-        }
-        return [ error: "You must supply a name" ]
-    }
-    /**
-     * Finds a Job History by name
-     * 
-     * @param     name    The unique name of the job history
-     * @return            An object containing the job history
-     */
-    def findJobHistory(String name) {
-        if(!!name) {
-            def jobHistory = JobHistory.findByName(name.trim())
-            if(!!jobHistory) {
-                return [ jobHistory: jobHistory ]
-            }
-            return [ error : "Job History named ${name} not found!"]            
-        }
-        return [ error: "You must supply a name" ]
-    }
-    /**
      * Retrieves a paginated list of job logs for a specified job history
      * 
      * @param     name     The unique name of the job history
@@ -66,29 +33,34 @@ class JobService {
      */
     def getJobLogs(String name,Integer records = 20,Integer offset = 0) {
         if(!!name) {
-            def jobHistory = JobHistory.findByName(name.trim())
-            if(!!jobHistory) {
-                return [
-                    jobLogs: JobLog.createCriteria().list(sort: 'id', order:'desc', max: records, offset: offset) {
-                        eq('jobHistory',jobHistory)
-                        if(!(GrailsUtil.environment in ['test'])) {
-                            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
-                            projections {
-                                property('logTime', 'logTime')
-                                property('line', 'line')
-                                property('id','id')
-                            }
+            def (chain, suffix) = name.tokenize( ':' )
+            return [
+                jobLogs: JobEventLog.createCriteria().list(sort: 'id', order:'desc', max: records, offset: offset) {
+                    eq('scheduledChain',chain)
+                    eq('scheduledUniqueJobId',suffix)
+                    if(!(GrailsUtil.environment in ['test'])) {
+                        resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+                        projections {
+                            property('dateCreated', 'logTime')
+                            property('status', 'status')
+                            property('currentOperation','currentOperation')                                                        
+                            property('id','id')
                         }
-                    },
-                    jobHistories: getJobHistories().jobHistories,
-                    total: JobLog.countByJobHistory(jobHistory)
-                ]
-            }
-            return [ error : "Job History named ${name} not found!"]  
+                    }
+                }.collect { jel ->
+                    if(GrailsUtil.environment in ['test']) {
+                        jel.logTime = jel.dateCreated
+                    }
+                    jel.line = "[${jel.currentOperation}] ${jel.status}"
+                    return (GrailsUtil.environment in ['test'])?jel.properties['logTime','line','id']:jel.subMap(['logTime','line','id'])                            
+                },
+                jobHistories: getJobHistories().jobHistories,
+                total: JobEventLog.countByScheduledChainAndScheduledUniqueJobId(chain,suffix)
+            ]
         }
         return [ error: "You must supply a name" ]
     }
-    /**
+   /**
      * Retrieves a paginated list of calculated job timings for a specified job history
      * 
      * @param     records  The number of records to return
@@ -97,60 +69,44 @@ class JobService {
      */
     def getJobRuleTimings(String name,Integer records = 20,Integer offset = 0) {
         if(!!name) {
-            def jobHistory = JobHistory.findByName(name.trim())
-            if(!!jobHistory) {
-                def endTime
-                return [
-                    jobLogs: { jls ->                        
-                        endTime = JobLog.createCriteria().get {
-                            eq('jobHistory',jobHistory)
-                            if(!!jls) {
-                                gt('id',jls.last().id)
-                            }
-                            or {
-                                like('line','[%] Detected a % for%')
-                                like('line','[Finished] %')
-                            }
-                            projections {
-                                min('logTime')
-                            }
-                        }
-                        if(!!!endTime) {
-                            endTime = JobLog.createCriteria().get {
-                                eq('jobHistory',jobHistory)
-                                if(!!jls) {
-                                    gt('id',jls.last().id)
-                                }
-                                projections {
-                                    min('logTime')
-                                }
-                            }                                                        
-                        }
-                        return jls.reverse().collect { jl ->
-                            jl.duration = TimeCategory.minus(endTime, jl.logTime).toString()
-                            endTime = jl.logTime
-                            jl.ruleName = jl.line.tokenize().last()
-                            return jl                            
-                        }.reverse()                        
-                    }.call(JobLog.createCriteria().list(sort: 'id', order:'asc', max: records, offset: offset) {
-                        eq('jobHistory',jobHistory)
-                        like('line','[%] Detected a % for%')
-                        if(!(GrailsUtil.environment in ['test'])) {
-                            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
-                            projections {
-                                property('logTime', 'logTime')
-                                property('line', 'line')
-                                property('id','id')
-                            }
-                        }
-                    }),
-                    jobHistories: getJobHistories().jobHistories,
-                    total: JobLog.countByJobHistoryAndLineLike(jobHistory,'[%] Detected a % for%')
-                ]
+            def (chain, suffix) = name.tokenize( ':' )
+            def ruleFilter = ['Groovy','Python','Ruby','SQLQuery','StoredProcedureQuery','DefinedService','Snippet']
+            def endTime = JobEventLog.createCriteria().get {
+                eq('scheduledChain',chain)
+                eq('scheduledUniqueJobId',suffix)
+                projections {
+                    max("dateCreated")
+                }
             }
-            return [ error : "Job History named ${name} not found!"]             
+            return [
+                jobLogs: JobEventLog.createCriteria().list(sort: 'id', order:'asc', max: records, offset: offset) {
+                    eq('scheduledChain',chain)
+                    eq('scheduledUniqueJobId',suffix)
+                    inList('currentOperation',ruleFilter)
+                    like('status','Detected a % for%')
+                    if(!(GrailsUtil.environment in ['test'])) {
+                        resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+                        projections {
+                            property('dateCreated', 'logTime')
+                            property('status', 'status')
+                            property('currentOperation','currentOperation')                            
+                            property('id','id')
+                        }
+                    }
+                }.reverse().collect { jl ->
+                    if(GrailsUtil.environment in ['test']) {
+                        jl.logTime = jl.dateCreated
+                    }
+                    jl.duration = TimeCategory.minus(endTime, jl.logTime).toString()
+                    endTime = jl.logTime
+                    jl.ruleName = jl.status.tokenize().last()
+                    jl.line = "[${jl.currentOperation}] ${jl.status}"
+                    return (GrailsUtil.environment in ['test'])?jl.properties['logTime','duration','ruleName','line','id']:jl.subMap(['logTime','duration','ruleName','line','id'])                            
+                }.reverse(),
+                jobHistories: getJobHistories().jobHistories,
+                total: JobEventLog.findAllByScheduledChainAndScheduledUniqueJobIdAndStatusLikeAndCurrentOperationInList(chain,suffix,'Detected a % for%',ruleFilter).size()
+            ]
         }
-        return [ error: "You must supply a name" ]
     }
     /**
      * Returns a list of available Job Histories
@@ -158,37 +114,31 @@ class JobService {
      * @return     A list of job histories
      */
     def getJobHistories() {
-        return [ 
-            jobHistories: (GrailsUtil.environment in ['test'])?JobHistory.list():JobHistory.withCriteria {
-                resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
-                projections {
-                    property('id', 'id')
-                    property('name', 'name')
-                    property('chain', 'chain')
-                    property('groupName', 'groupName')
-                    property('description', 'description')
-                    property('cron', 'cron')
-                    property('fireTime', 'fireTime')
-                    property('scheduledFireTime', 'scheduledFireTime')
-                }                
-            }.collect { jh ->
-                def jobHistory = JobHistory.findByName(jh.name)
-                jh.endTime = JobLog.createCriteria().get {
-                    eq('jobHistory',jobHistory)
+        return [
+            jobHistories: (GrailsUtil.environment in ['test'])?JobEventLog.list():JobEventLog.createCriteria().listDistinct() { 
+                eq('currentOperation','SUMMARY')
+            }.collect { jel ->
+                def jh = jel.getJobInfo()
+                jh.endTime = JobEventLog.createCriteria().get {
+                    eq('scheduledChain',jel.scheduledChain)
+                    eq('scheduledUniqueJobId',jel.scheduledUniqueJobId)
                     projections {
-                        max("logTime")
+                        max("dateCreated")
                     }
                 }
-                jh.startTime = JobLog.createCriteria().get {
-                    eq('jobHistory',jobHistory)
+                jh.startTime = JobEventLog.createCriteria().get {
+                    eq('scheduledChain',jel.scheduledChain)
+                    eq('scheduledUniqueJobId',jel.scheduledUniqueJobId)
                     projections {
-                        min("logTime")
+                        min("dateCreated")
                     }
                 }
                 jh.duration = TimeCategory.minus(jh.endTime, jh.startTime).toString()
+                jh.name = "${jel.scheduledChain}:${jel.scheduledUniqueJobId}"
+                jh.id = jel.id
                 return jh
             }
-        ]
+        ]            
     }
     /**
      * Removes a specified Job History by name
@@ -198,12 +148,13 @@ class JobService {
      */
     def deleteJobHistory(String name) {
         if(!!name) {
-            def jobHistory = JobHistory.findByName(name.trim())
-            if(!!jobHistory) {
-                jobHistory.delete()
-                return [ success : "Job History deleted" ]
-            }
-            return [ error : "Job History named ${name} not found!"]
+            def (chain, suffix) = name.tokenize( ':' )
+            def recordsDeleted = JobEventLog.where { 
+                (scheduledChain == chain && scheduledUniqueJobId == suffix)
+            }.deleteAll()
+            return [
+                success: "Job History deleted for ${chain}:${suffix} with ${recordsDeleted} records deleted"
+            ]
         }
         return [ error: "You must supply a name" ]
     }
