050da19b578716ea5ae453c1660e6b0780e71af9
James Jones <jamjon3@gmail.com>
2014-10-08 10:57:31 -0400

    Added more log breakdown fields parsed from the log line

--- grails-app/domain/edu/usf/RuleChains/JobEventLog.groovy
+++ grails-app/domain/edu/usf/RuleChains/JobEventLog.groovy
@@ -1,5 +1,7 @@
 package edu.usf.RuleChains
 
+import grails.converters.*
+
 class JobEventLog {
 
     final static int SOURCE_MAXSIZE = 255
@@ -11,17 +13,37 @@ class JobEventLog {
     String message
     String details
     String source
+    String scheduledChain
+    String scheduledUniqueJobId
+    String currentRunningChain
+    String currentOperation
+    String status    
  
     // did someone look at this error?
     boolean cleared = false
- 
+    
+    static transients = ['jobInfo']
+    
     static constraints = {
         source(blank: false, nullable: false, maxSize: SOURCE_MAXSIZE)
         message(blank: false, nullable: false, maxSize: MESSAGE_MAXSIZE)
         details(blank: true, nullable: true, maxSize: DETAILS_MAXSIZE)
+        scheduledChain(blank: true, nullable: true)
+        scheduledUniqueJobId(blank: true, nullable: true)
+        currentRunningChain(blank: true, nullable: true)
+        currentOperation(blank: true, nullable: true)
+        status(blank:true)
     }
  
     static mapping = {
+        status type: 'text'
         sort "dateCreated"
-    }
+    }        
+    
+    def getJobInfo() {
+        if(currentOperation == "SUMMARY") {
+            return JSON.parse(status.minus("JobInfo "))
+        }
+        return [:]
+    }    
 }
