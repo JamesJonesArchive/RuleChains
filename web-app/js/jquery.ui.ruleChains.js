eb81d6080cbd9751b65293c520eb405b9bd6464b
james <james@james.it.usf.edu>
2014-11-10 12:14:31 -0500

    Moved the wait dialog into the plugin

--- web-app/js/jquery.ui.ruleChains.js
+++ web-app/js/jquery.ui.ruleChains.js
@@ -13,7 +13,13 @@
         _create: function() {
             var self = this,
                 o = self.options,
-                el = self.element;
+                el = self.element,
+                waitDialog = self.waitDialog;
+            self.getWaitDialog().ajaxStart(function() { 
+                $(this).dialog("open");
+            }).ajaxStop(function() {
+                $(this).dialog("close");
+            });
             $.ajaxSetup({
                 dataType : "json",
                 type: "POST",
@@ -55,7 +61,57 @@
                 }
             });
             self.buildTabContent();            
-        },      
+        },  
+        getWaitDialog: function() {
+            var self = this;
+            if(typeof self.waitDialog === 'undefined') {
+                self.waitDialog = $('<div />');
+                self.waitDialog
+                .addClass('ui-state-default ui-widget-content')
+                .css({
+                    "background": 'url("images/ajax-loader.gif") no-repeat scroll 50% 50% rgba(0, 0, 0, 0)',
+                    "background-position": "center center"
+                })
+                .append(
+                    $('<p />')
+                    .css({
+                        "text-align": "center",
+                        "margin-top": "0px",
+                        "margin-bottom": "0px",
+                        "padding": "0px"
+                    })
+                ).dialog({            
+                    autoOpen: false,
+                    draggable: false,
+                    resizable: false,
+                    show: {
+                        effect: 'fade',
+                        duration: 200
+                    },
+                    hide: {
+                        effect: 'fade',
+                        duration: 200
+                    },
+                    open: function(){
+                    },
+                    close: function(){
+                    },            
+                    bgiframe: true,
+                    title: 'Please Wait....',
+                    height:180,
+                    width:220,
+                    modal: true,
+                    zIndex: 3999,
+                    overlay: {
+                        backgroundColor: '#000',
+                        opacity: 0.5
+                    }
+                });
+                return self.waitDialog;
+            } else {
+                return self.waitDialog;
+            }
+        },
         buildTabContent: function() {
             var self = this,
                 o = self.options,
